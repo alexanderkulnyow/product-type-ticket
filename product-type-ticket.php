1b3dedd04477e112900709e635aa3262552d1f76
alexander <alexanderkulnyow@yahoo.com>
2020-05-03 11:28:02 +0300

    init1

---
+++ product-type-ticket.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+ * Plugin Name: Product type "Ticket"
+ * Description: Информация о победителях конкурса со сроком действия
+ * Plugin URI:  https://github.com/alexanderkulnyow/dance_queen
+ * Author URI:  Ссылка на автора
+ * Author:      alexander kulnyow
+ *
+ * Text Domain: ID перевода. Пр: my-plugin
+ * Domain Path: Путь до MO файла (относительно папки плагина)
+ *
+ * Requires PHP: 5.4
+ * Requires at least: 2.5
+ *
+ * License:     GPL2
+ * License URI: https://www.gnu.org/licenses/gpl-2.0.html
+ *
+ * Network:     true - активирует плагин для всей сети
+ * Version:     1.0
+ */
